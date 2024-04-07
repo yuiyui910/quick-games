be31ddb8bb9802782be71223e7d0cf80b4e017c4
yuiyui910 <yui1423@icloud.com>
2024-04-07 17:57:29 +0900

    不要なimportの削除

--- src/features/top/components/GameInfoField.tsx
+++ src/features/top/components/GameInfoField.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { gameInfo } from "types/gameInfo";
 
 export const GameInfoField = (gameInfo: gameInfo) => {
